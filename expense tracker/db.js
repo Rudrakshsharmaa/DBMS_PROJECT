814ecb2384e413dc3d54723cff2074c847b46273
Rudrakshsharmaa <rudrakshsharma0377@gmail.com>
2026-03-25 00:48:59 +0530

    Update db.js

--- expense tracker/db.js
+++ expense tracker/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 
 // Use connection pool (better than single connection)
 const db = mysql.createPool({
-    host: process.env.DB_HOST || 'your-host',
-    user: process.env.DB_USER || 'your-user',
-    password: process.env.DB_PASSWORD || 'your-password',
-    database: process.env.DB_NAME || 'your-database',
-    port: process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST || 'dpg-d71e3414tr6s73am7r00-a',
+    user: process.env.DB_USER || 'expense_db_vacb_user',
+    password: process.env.DB_PASSWORD || '55hFvAF25nqsIprYmitLP9vmKAaMQqkB',
+    database: process.env.DB_NAME || ' expense_db_vacb ',
+    port: process.env.DB_PORT || '5432',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
